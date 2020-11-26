a3e675633a49cba30bdd9ea6c7ab390fb33fe0a8
vlad6243 <44611772+vlad6243@users.noreply.github.com>
2020-11-26 16:38:48 +0200

    fix hw2

--- hw2/webpack.config.js
+++ hw2/webpack.config.js
@@ -1,41 +1,41 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: "./src/index.js",
-    mode: 'development',
-    output: {
-        path: path.join(__dirname, "/dist"),
-        filename: "index_bundle.js"
-    },
-    module: {
-        rules:[
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: ["style-loader", "css-loader", "sass-loader",]
-            },
-            {
-                test: /\.less$/,
-                loader: "less-loader", // compiles Less to CSS
-            },
-            {
-                test: /\.js$|jsx/,
-                exclude: /node_modules/,
-                use: ["babel-loader", "eslint-loader"]
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/i,
-                loader: "file-loader"
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        })
-    ]
+  entry: './src/index.jsx',
+  mode: 'development',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index_bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.less$/,
+        loader: 'less-loader', // compiles Less to CSS
+      },
+      {
+        test: /\.js$|jsx/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: 'file-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
 };
